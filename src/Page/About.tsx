d82d0b729b87ba46e01057bd1817cd55985ec2f7
Oyebowale Azeez <Oyebowale@relianceinfosystems.com>
2026-01-17 01:44:38 +0100

    added popup for subscriber on about page removed auth

--- src/Page/About.tsx
+++ src/Page/About.tsx
@@ -378,7 +378,7 @@ export default function About() {
 <SubscribeModal
         open={open}
         onClose={() => setOpen(false)}
-        endpoint="/subscribers/all"
+        endpoint="/subscribe"
         onSuccess={() => {
           // optional: close after success
           // setTimeout(() => setOpen(false), 1500);
