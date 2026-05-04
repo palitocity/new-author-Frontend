ae94793d7aad24535d3bb39d741cb1cb60fbb3d0
Dhanielgeek <dhanielknightz46@gmail.com>
2026-05-04 18:22:39 +0100

    new updated for the verify

--- src/Page/Verifypayment.tsx
+++ src/Page/Verifypayment.tsx
@@ -1,18 +1,18 @@
-/* eslint-disable react-hooks/set-state-in-effect */
 import { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import axios from "../config/axiosconfiq";
 
 export default function VerifyPayment() {
   const [status, setStatus] = useState("Verifying...");
-  const navigate = useNavigate();
+  const [success, setSuccess] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
-    const reference = queryParams.get("reference"); // get from query
+    const reference = queryParams.get("reference");
 
     if (!reference) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       setStatus("No payment reference found.");
       return;
     }
@@ -20,9 +20,10 @@ export default function VerifyPayment() {
     const verifyPayment = async () => {
       try {
         const res = await axios.get(`/transactions/verify/${reference}`);
+
         if (res.data.success) {
-          setStatus("Payment successful! Order confirmed.");
-          setTimeout(() => navigate("/library"), 3000);
+          setSuccess(true);
+          setStatus("Payment successful!");
         } else {
           setStatus("Payment failed or pending.");
         }
@@ -33,11 +34,28 @@ export default function VerifyPayment() {
     };
 
     verifyPayment();
-  }, [location.search, navigate]);
+  }, [location.search]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p className="text-center text-xl">{status}</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      {!success ? (
+        <p className="text-xl">{status}</p>
+      ) : (
+        <div className="bg-white p-8 rounded-2xl shadow-lg text-center max-w-md">
+          <h2 className="text-2xl font-bold mb-4">🎉 Payment Successful</h2>
+          <p className="text-gray-600 mb-4">Your purchase was successful.</p>
+          <p className="text-gray-800 font-medium">
+            Kindly check your email for your stories 📚
+          </p>
+
+          <button
+            onClick={() => (window.location.href = "/")}
+            className="mt-6 px-6 py-2 bg-black text-white rounded-lg"
+          >
+            Go Home
+          </button>
+        </div>
+      )}
     </div>
   );
 }
