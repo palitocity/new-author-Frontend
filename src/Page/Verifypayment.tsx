14f712774844028c4aea207e9911fed9186c6b99
Dhanielgeek <dhanielknightz46@gmail.com>
2026-05-13 15:03:56 +0100

    updated the change to confirm...

--- src/Page/Verifypayment.tsx
+++ src/Page/Verifypayment.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/set-state-in-effect */
 import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
@@ -20,15 +21,24 @@ export default function VerifyPayment() {
     const verifyPayment = async () => {
       try {
         const res = await axios.get(`/transactions/verify/${reference}`);
+
+        console.log("🔥 VERIFY RESPONSE:", res);
+        console.log("🔥 VERIFY DATA:", res.data);
         if (res.data.success) {
           setStatus("Payment successful! Order confirmed.");
           setTimeout(() => navigate("/library"), 3000);
         } else {
           setStatus("Payment failed or pending.");
         }
-      } catch (err) {
-        setStatus("Error verifying payment.");
-        console.log(err);
+      } catch (err: any) {
+        console.log("❌ VERIFY ERROR:", err);
+        console.log("❌ ERROR RESPONSE:", err?.response?.data);
+
+        setStatus(
+          err?.response?.data?.error ||
+            err.message ||
+            "Error verifying payment.",
+        );
       }
     };
 
