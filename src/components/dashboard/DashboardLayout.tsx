ed30e60a3f5b930606cc854f7cf758792456df8c
Dhanielgeek <dhanielknightz46@gmail.com>
2026-09-05 10:23:11 +0100

    test again

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -30,7 +30,7 @@ const navItems = [
     to: "/dashboard/reflection-notes",
     icon: NotebookPen,
   },
-  // { label: "Bookmarks", to: "/dashboard/bookmarks", icon: BookMarked },
+
   { label: "Purchase History", to: "/dashboard/purchases", icon: Clock3 },
   // { label: "Saved Stories", to: "/dashboard/saved", icon: BookMarked },
   { label: "Profile Settings", to: "/dashboard/profile", icon: Settings },
